4124420064565fe287b3208d01ba8e4410718912
demasammour <demasammour@gmail.com>
2023-01-11 16:27:27 +0300

    confirm

--- Scripts/Import Compaines/Test Case 001 - Open GC - Copy/Script1672738792200.groovy
+++ Scripts/Import Compaines/Test Case 001 - Open GC - Copy/Script1672738792200.groovy
@@ -39,11 +39,12 @@ String dirName = System.getProperty('user.dir')
 
 //to print the name on consol and check it 
 println('$$$$$' + dirName)
+
 println('$$$$$' + randomstring)
 
-String excelFile01 = ("Data Files//" + randomstring + "xlsx")
-//String excelFile01 = randomstring + '.xlsx'
+String excelFile01 = ('Data Files//' + randomstring) + '.xlsx'
 
+//String excelFile01 = randomstring + '.xlsx'
 String excelFile2 = randomstring
 
 println('$$$$$' + excelFile01)
@@ -74,25 +75,74 @@ ExcelKeywords.setValueToCellByIndex(sheet0, 0, 1, 'Legal name')
 
 ExcelKeywords.setValueToCellByIndex(sheet0, 0, 2, 'Common Names')
 
+ExcelKeywords.setValueToCellByIndex(sheet0, 0, 3, 'Industries')
+
+ExcelKeywords.setValueToCellByIndex(sheet0, 0, 4, 'Asset Classes')
+
+ExcelKeywords.setValueToCellByIndex(sheet0, 0, 5, 'Source')
+
+ExcelKeywords.setValueToCellByIndex(sheet0, 0, 6, 'Company ID')
+
+ExcelKeywords.setValueToCellByIndex(sheet0, 0, 7, 'LinkedIn URL')
+
+ExcelKeywords.setValueToCellByIndex(sheet0, 0, 8, 'Website URL')
+
+ExcelKeywords.setValueToCellByIndex(sheet0, 0, 9, 'Team URL')
+
+ExcelKeywords.setValueToCellByIndex(sheet0, 0, 10, 'Contact URL')
+
+ExcelKeywords.setValueToCellByIndex(sheet0, 0, 11, 'Portfolio URL')
+
+ExcelKeywords.setValueToCellByIndex(sheet0, 0, 12, 'HQ Address 1')
+
+ExcelKeywords.setValueToCellByIndex(sheet0, 0, 13, 'HQ Address 2')
+
+ExcelKeywords.setValueToCellByIndex(sheet0, 0, 14, 'HQ Locality')
+
+ExcelKeywords.setValueToCellByIndex(sheet0, 0, 15, 'HQ Region')
+
+ExcelKeywords.setValueToCellByIndex(sheet0, 0, 16, 'HQ Postal Code')
+
+ExcelKeywords.setValueToCellByIndex(sheet0, 0, 17, 'HQ Country')
+
+ExcelKeywords.setValueToCellByIndex(sheet0, 0, 18, 'Reviewed At')
+
+ExcelKeywords.setValueToCellByIndex(sheet0, 0, 19, 'Status Bisnow')
+
+ExcelKeywords.setValueToCellByIndex(sheet0, 0, 20, 'Operating Status')
+
+CustomKeywords.'com.kms.katalon.keyword.excel.ExcelKeywords.setValueToCellByAddress'(sheet0, 'A2', 'Hickory Capital Group')
+
+CustomKeywords.'com.kms.katalon.keyword.excel.ExcelKeywords.setValueToCellByAddress'(sheet0, 'B2', 'Hickory Capital Group, LLC')
+
+CustomKeywords.'com.kms.katalon.keyword.excel.ExcelKeywords.setValueToCellByAddress'(sheet0, 'C2', 'Hickory Capital Group')
+
+CustomKeywords.'com.kms.katalon.keyword.excel.ExcelKeywords.setValueToCellByAddress'(sheet0, 'D2', 'Real Estate Investment Firm ,Developer')
+
 // Save data of File01
 ExcelKeywords.saveWorkbook(excelFile01, workbook01)
 
 //Get name of sheets in workbook
 String sheetname = ExcelKeywords.getSheetNames(workbook01)
 
-WebUI.sendKeys(findTestObject('Import Companies/choose to upload file - input file'), dirName +"/"+
-excelFile01)
+System.out.println(sheetname)
 
+WebUI.sendKeys(findTestObject('Import Companies/choose to upload file - input file'), (dirName + '//') + excelFile01)
 
 //WebUI.uploadFile(findTestObject('Import Companies/Choose to upload file - input file'), ((dirName + '\\') + excelFile2) + 
-   // '.xlsx')
+  //  '.xlsx')
 
 WebUI.click(findTestObject('Import Companies/Import compines button'))
 
 WebUI.click(findTestObject('Import Companies/here link'))
 
 // Delete file
-
+//file1.delete()
 
 //close browser
 WebUI.closeBrowser()
+
+
+
+
+//// this code need to download  liabries  >> check this >> https://store.katalon.com/product/34/Excel-Keywords#overview-content and https://github.com/katalon-studio/katalon-excel-keyword/blob/master/ExcelPluginSample/Drivers/ExcelKeywords.jar and http://www.java2s.com/Code/Jar/j/Downloadjxl26jar.htm%E2%80%9D
